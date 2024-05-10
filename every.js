e3d6f198b9677bf7ef3cc519a45c5fa37f034301
ntwalii <ntwaliaubin@gmail.com>
2024-05-10 09:11:04 +0200

    Every and includes built ins

---
+++ every.js
@@ -0,0 +1,6 @@
+//every() returns a boolean if all elements in an array pass a certain condition
+
+let arr=[1,2,3,4,5];
+
+console.log(arr.every(elt=>elt>0))
+console.log(arr.every(elt=>elt<0))
---
+++ includes.js
@@ -0,0 +1,7 @@
+let str= "Aubin";
+let arr=[1,2,3,4];
+
+console.log(str.includes("A"));
+console.log(arr.includes(2));
+console.log(str.includes("b",4));//passing another argument that represents the index to start searching from
+console.log(arr.includes(2,0));
